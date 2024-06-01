b47e86e71220cdd1163930a8028749f5885479bd
Matthew Gibson <mgibson@microsoft.com>
2024-06-01 18:34:38 -0400

    close scanner

--- ch_18/Exercise18_01.java
+++ ch_18/Exercise18_01.java
@@ -6,10 +6,12 @@ import java.util.Scanner;
 import static resources.lib.UnitTest.*;
 
 /**
- * *18.1 (Factorial) Using the BigInteger class introduced in Section 10.9, you can
+ * *18.1 (Factorial) Using the BigInteger class introduced in Section 10.9, you
+ * can
  * find the factorial for a large number (e.g., 100!).
  * Implement the factorial method using recursion.
- * Write a program that prompts the user to enter an integer and displays its factorial.
+ * Write a program that prompts the user to enter an integer and displays its
+ * factorial.
  */
 public class Exercise18_01 {
 
@@ -26,8 +28,9 @@ public class Exercise18_01 {
 
             System.out.println(numToFactor + "! is " + findFactorial(numToFactor));
         }
-    }
+        in.close();
 
+    }
 
     static BigInteger findFactorial(long num) {
         long startIterateNum = 1;
@@ -50,7 +53,8 @@ public class Exercise18_01 {
     private static class Test {
 
         public static boolean testFactorial() {
-            BigInteger correctResult = new BigInteger("30414093201713378043612608166064768844377641568960512000000000000");
+            BigInteger correctResult = new BigInteger(
+                    "30414093201713378043612608166064768844377641568960512000000000000");
             BigInteger res = Exercise18_01.findFactorial(50);
             try {
                 assertEquals(correctResult, res);
@@ -64,5 +68,3 @@ public class Exercise18_01 {
 
     }
 }
-
-
